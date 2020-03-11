4d46aea10ffb517ade0082bcb046f15650cb1739
Leandro Venâncio <leandrohilariovenancio@gmail.com>
2020-03-10 22:46:07 -0300

    Aula 07: Operador resto e operadores de comparação

---
+++ src/OperadoresDeComparacao.java
@@ -0,0 +1,13 @@
+public class OperadoresDeComparacao {
+    public static void main(String[] args) {
+        boolean dezMaiorQueVinte = 10 > 20;
+        boolean dezMaiorIgualaVinte = 10 >= 20;
+        boolean dezMenorQueVinte = 10 < 20;
+        boolean dezMenorIgualAVinte = 10 <= 20;
+
+        System.out.println("Dez maior que vinte " + dezMaiorQueVinte);
+        System.out.println("Dez maior igual a vinte " + dezMaiorIgualaVinte);
+        System.out.println("Dez menor que vinte " + dezMenorQueVinte);
+        System.out.println("Dez menor igual a vinte " + dezMenorIgualAVinte);
+    }
+}
